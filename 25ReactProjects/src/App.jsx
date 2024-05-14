4bfd3fe166da5c860d33b074c0bd4b60f6d237c8
nelg62 <glen.harding.nz@gmail.com>
2024-05-15 11:56:05 +1200

    random color generator done

--- 25ReactProjects/src/App.jsx
+++ 25ReactProjects/src/App.jsx
@@ -3,12 +3,16 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Accordian from "./components/accordion";
+import RandomColor from "./components/random-color";
 
 function App() {
   return (
     <>
       {/* accordian component */}
-      <Accordian></Accordian>
+      {/* <Accordian></Accordian> */}
+
+      {/* random Color component */}
+      <RandomColor></RandomColor>
     </>
   );
 }
