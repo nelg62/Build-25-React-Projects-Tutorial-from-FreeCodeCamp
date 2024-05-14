60b5df55070cf114d3ac449047e29df4783c8401
nelg62 <glen.harding.nz@gmail.com>
2024-05-15 11:18:58 +1200

    Accordian Project done some tings to fix e.g id error and when single select is open and changing to milti select cannot change the view

---
+++ 25ReactProjects/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Accordian from "./components/accordion";
+
+function App() {
+  return (
+    <>
+      {/* accordian component */}
+      <Accordian></Accordian>
+    </>
+  );
+}
+
+export default App;
---
+++ 25ReactProjects/src/components/accordion/data.jsx
@@ -0,0 +1,20 @@
+const data = [
+  { id: "1", question: "What is the capital of France?", answer: "Paris" },
+  {
+    id: "2",
+    question: "Who wrote 'Romeo and Juliet'?",
+    answer: "William Shakespeare",
+  },
+  {
+    id: "3",
+    question: "What is the chemical symbol for water?",
+    answer: "H2O",
+  },
+  {
+    id: "4",
+    question: "Who painted the Mona Lisa?",
+    answer: "Leonardo da Vinci",
+  },
+];
+
+export default data;
