7d7b7146d82e75ca1521af8ee5ecbb2d587fe32a
nelg62 <glen.harding.nz@gmail.com>
2024-05-15 12:39:36 +1200

    star rating done

--- 25ReactProjects/src/App.jsx
+++ 25ReactProjects/src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import Accordian from "./components/accordion";
 import RandomColor from "./components/random-color";
+import StarRating from "./components/star-rating";
 
 function App() {
   return (
@@ -12,7 +13,10 @@ function App() {
       {/* <Accordian></Accordian> */}
 
       {/* random Color component */}
-      <RandomColor></RandomColor>
+      {/* <RandomColor></RandomColor> */}
+
+      {/* Star rating component */}
+      <StarRating noOfStars={10}></StarRating>
     </>
   );
 }
