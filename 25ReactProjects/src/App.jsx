823d74c4687c9c9a02610f5ec07048c620a3ce9d
nelg62 <glen.harding.nz@gmail.com>
2024-05-15 15:02:35 +1200

    image slider done

--- 25ReactProjects/src/App.jsx
+++ 25ReactProjects/src/App.jsx
@@ -5,6 +5,7 @@ import "./App.css";
 import Accordian from "./components/accordion";
 import RandomColor from "./components/random-color";
 import StarRating from "./components/star-rating";
+import ImageSlider from "./components/image-slider";
 
 function App() {
   return (
@@ -16,7 +17,14 @@ function App() {
       {/* <RandomColor></RandomColor> */}
 
       {/* Star rating component */}
-      <StarRating noOfStars={10}></StarRating>
+      {/* <StarRating noOfStars={10}></StarRating> */}
+
+      {/* image Slider component */}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
     </>
   );
 }
